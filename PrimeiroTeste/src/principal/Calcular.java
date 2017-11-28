73cec5efed6112d4551a6efacfaa4d1fb8f61096
maycon <mayconn.aguiarr@gmail.com>
2017-11-27 20:10:54 -0500

    realizado o teste da classe Calcular

---
+++ PrimeiroTeste/src/principal/Calcular.java
@@ -0,0 +1,17 @@
+package principal;
+
+public class Calcular {
+
+	public int soma(int a, int b) {
+		return a + b;
+	}
+
+	public int subtracao(int a, int b) {
+		return a - b;
+	}
+
+	public boolean calcularMultiplo(int numero) {
+		return numero % 3 == 0;
+	}	
+	
+}
